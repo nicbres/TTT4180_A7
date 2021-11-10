2601e638e6498b23263c81f41880689c2fb2be71
Nicholas Bresina <nicholdb@stud.ntnu.no>
2021-11-10 15:45:25 +0100

    Update run TLM script
    Adds an impulse function to be used. Additionally introduces skipping of
    draw frames and clearing of draw buffer.

--- Code/run_tlm.py
+++ Code/run_tlm.py
@@ -11,40 +11,53 @@ from tlm.source import Source
 
 
 def harmonic_function(time: float):
-    return np.cos(2 * np.pi * 500 * time)
+    return np.sin(2 * np.pi * 500 * time)
+
+
+def impulse_function(time: float):
+    if time == 0:
+        return 1.0
+    else:
+        return 0.
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
 
     sources = [
+        #Source(coordinates=(0, 0), function=impulse_function),
         Source(coordinates=(0, 0), function=harmonic_function),
     ]
 
     res_tlm = TLM(
         maximum_frequency=2000,
         sound_speed=343,
-        length=1,
+        length=2,
         width=0.2,
         sources=sources,
         wavelength_delta_x_ratio=10,
+        reflection_coefficient_right=0.1,
     )
 
     fig, ax = plt.subplots()
 
-    norm = colors.Normalize(vmin=-2.0, vmax=2.0)
+    norm = colors.Normalize(vmin=-1.0, vmax=1.0)
     im = ax.imshow(res_tlm.get_pressure_layer().T, norm=norm)
 
     def animate(*args, **kwargs):
+        ax.clear()
         start_time = time.time()
-        res_tlm.update()
-        logging.debug(f"Time used for Update: {time.time() - start_time:.6f} s")
+        for _ in range(3):
+            res_tlm.update()
+        update_time = time.time()
         im = ax.imshow(res_tlm.get_pressure_layer().T, norm=norm)
+        draw_time = time.time()
+        logging.debug(f"Update: {update_time - start_time:.6f} s  |  Draw {draw_time - update_time:.6f}")
         return im,
 
 
     ani = animation.FuncAnimation(
-        fig, animate, interval=1, save_count=30,
+        fig, animate, interval=10,
     )
 
     plt.show()
