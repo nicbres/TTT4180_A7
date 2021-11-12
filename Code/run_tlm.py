508a24762b16a9bba56ea530da9aaf95134afff4
Nicholas Bresina <nicholdb@stud.ntnu.no>
2021-11-12 23:32:39 +0100

    Add cleanup

--- Code/run_tlm.py
+++ Code/run_tlm.py
@@ -20,6 +20,43 @@ def impulse_function(time: float):
         return 0.
 
 
+def plot(tlm: TLM, cnorm: colors.Normalize):
+    fig, ax = plt.subplots()
+    im = ax.imshow(tlm.get_pressure_layer(), norm=cnorm)
+
+    def animate(*args, **kwargs):
+        for _ in np.arange(4):
+            res_tlm.update()
+        ax.clear()
+        pressure_layer = res_tlm.get_pressure_layer()[::-1, :]
+        im = ax.imshow(pressure_layer, norm=cnorm)
+        return im,
+
+    ani = animation.FuncAnimation(
+        fig, animate, interval=5,
+    )
+
+    plt.show()
+
+
+def generate_images(
+    tlm: TLM,
+    cnorm: colors.Normalize,
+    colormap: str = 'inferno',
+    number_of_images: int = 10,
+    skip_rate: int = 80,
+):
+    colormap = plt.get_cmap(colormap)
+    scalar_mappable = cm.ScalarMappable(norm=cnorm, cmap=colormap)
+
+    for i in range(number_of_images):
+        for _ in np.arange(skip_rate):
+            tlm.update()
+        pressure_layer = tlm.get_pressure_layer()[::-1, :]
+        image = scalar_mappable.to_rgba(pressure_layer)
+        imageio.imwrite(f'./images/pipe_{i}.png', image)
+
+
 if __name__ == "__main__":
     sources = [
         Source(coordinates=(0, 0), function=harmonic_function),
@@ -31,42 +68,12 @@ if __name__ == "__main__":
         length=2,
         width=0.2,
         sources=sources,
-        wavelength_delta_x_ratio=20,
-        reflection_coefficient_right=1.0,
+        wavelength_delta_x_ratio=10,
+        reflection_coefficient_right=0.1,
     )
-
-    fig, ax = plt.subplots()
 
     norm_max = 0.35
     norm = colors.Normalize(vmin=-norm_max, vmax=norm_max)
-    colormap = plt.get_cmap('inferno')
-    scalar_mappable = cm.ScalarMappable(norm=norm, cmap=colormap)
-    #im = ax.imshow(res_tlm.get_pressure_layer(), norm=norm)
 
-    """
-    def animate(*args, **kwargs):
-        for _ in np.arange(4):
-            res_tlm.update()
-        ax.clear()
-        pressure_layer = res_tlm.get_pressure_layer()[::-1, :]
-        if image_count < 100:
-            imageio.imwrite(f'./images/pipe_{image_count}', scalar_mappable.to_rgba(pressure_layer))
-            image_count += 1
-        im = ax.imshow(pressure_layer, norm=norm)
-        return im,
-    """
-
-    for i in range(20):
-        for _ in np.arange(80):
-            res_tlm.update()
-        pressure_layer = res_tlm.get_pressure_layer()[::-1, :]
-        image = scalar_mappable.to_rgba(pressure_layer)
-        imageio.imwrite(f'./images/pipe_{i}.png', image)
-
-"""
-    ani = animation.FuncAnimation(
-        fig, animate, interval=5,
-    )
-
-    plt.show()
-"""
+    #plot(tlm=res_tlm, cnorm=norm)
+    generate_images(tlm=res_tlm, cnorm=norm)
